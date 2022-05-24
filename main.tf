b29a7a6600284f75548ccc9524d29b21eb63fc45
Yota Toyama <raviqqe@gmail.com>
2022-05-24 01:53:19 +0000

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+variable "record_ttl" {
+  default = "300"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_route53_zone" "ein_com" {
+  name = "ein-lang.com"
+}
+
+resource "aws_route53_zone" "ein_org" {
+  name = "ein-lang.org"
+}
+
+resource "aws_route53_zone" "flame_com" {
+  name = "flame-lang.com"
+}
+
+resource "aws_route53_zone" "flame_org" {
+  name = "flame-lang.org"
+}
+
+resource "aws_route53_zone" "pen_com" {
+  name = "pen-lang.com"
+}
+
+resource "aws_route53_zone" "pen_org" {
+  name = "pen-lang.org"
+}
