82f6e84416d3b7e1a479c183a74786d16d026cd1
Yota Toyama <raviqqe@gmail.com>
2022-05-24 06:18:50 +0000

    Add free tier ec2 instance

--- machines.tf
+++ machines.tf
@@ -1,3 +1,29 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+}
+
 # TODO
 # resource "google_compute_instance" "xenon" {
 #   name         = "xenon"
