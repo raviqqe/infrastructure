f1a01ec05f31e005d222e2332120427d449c62be
Yota Toyama <raviqqe@gmail.com>
2022-05-24 05:47:03 +0000

    Disable xenon

--- machines.tf
+++ machines.tf
@@ -1,15 +1,16 @@
-resource "google_compute_instance" "xenon" {
-  name         = "xenon"
-  machine_type = "e2-highcpu-4"
-  zone         = "us-west1-b"
+# TODO
+# resource "google_compute_instance" "xenon" {
+#   name         = "xenon"
+#   machine_type = "e2-highcpu-4"
+#   zone         = "us-west1-b"
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-20"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "ubuntu-20"
+#     }
+#   }
 
-  network_interface {
-    network = "default"
-  }
-}
+#   network_interface {
+#     network = "default"
+#   }
+# }
