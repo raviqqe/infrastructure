d7ab20a3e35afc4baabce671989cdf5d8f05dfdf
Yota Toyama <raviqqe@gmail.com>
2022-05-23 23:55:10 -0700

    GCP (#2)
    * Add google provider
    
    * Add xenon
    
    * Move zone
    
    * Rename file
    
    * Disable xenon
    
    * Unset zone
    
    * Add domains module
    
    * Move record_ttl
    
    * Add free tier ec2 instance
    
    * Rename instance
    
    * Update filter
    
    * Refactor

---
+++ machines.tf
@@ -0,0 +1,51 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "argon" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+}
+
+# TODO
+# resource "google_compute_instance" "xenon" {
+#   name         = "xenon"
+#   machine_type = "e2-highcpu-4"
+#   zone         = "us-west1-b"
+
+#   boot_disk {
+#     initialize_params {
+#       image = "ubuntu-20"
+#     }
+#   }
+
+#   network_interface {
+#     network = "default"
+#   }
+# }
--- main.tf
+++ main.tf
@@ -8,54 +8,11 @@ terraform {
   }
 }
 
-variable "record_ttl" {
-  default = "300"
-}
-
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_route53_zone" "cloe_org" {
-  name = "cloe-lang.org"
-}
-
-resource "aws_route53_zone" "code2d_net" {
-  name = "code2d.net"
-}
-
-resource "aws_route53_zone" "code2d_org" {
-  name = "code2d.org"
-}
-
-resource "aws_route53_zone" "ein_com" {
-  name = "ein-lang.com"
-}
-
-resource "aws_route53_zone" "ein_org" {
-  name = "ein-lang.org"
-}
-
-resource "aws_route53_zone" "flame_com" {
-  name = "flame-lang.com"
-}
-
-resource "aws_route53_zone" "flame_org" {
-  name = "flame-lang.org"
-}
-
-resource "aws_route53_zone" "pen_com" {
-  name = "pen-lang.com"
-}
-
-resource "aws_route53_zone" "pen_org" {
-  name = "pen-lang.org"
-}
-
-resource "aws_route53_zone" "raviqqe_com" {
-  name = "raviqqe.com"
-}
-
-resource "aws_route53_zone" "ytoyama_com" {
-  name = "ytoyama.com"
+provider "google" {
+  project = "dev-server-199623"
+  region  = "us-west1"
 }
