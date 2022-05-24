4c363c86d6132e6b363e11d556737ea223b67e8e
Yota Toyama <raviqqe@gmail.com>
2022-05-24 06:23:58 +0000

    Rename instance

--- machines.tf
+++ machines.tf
@@ -19,7 +19,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "argon" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 }
