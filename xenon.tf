f024c4ec2747530539dc4ce1161684c586085290
Yota Toyama <raviqqe@gmail.com>
2022-05-28 13:38:02 -0700

    Security groups (#9)
    * Add security group
    
    * Refactor
    
    * Associate security group
    
    * Remove unused file
    
    * Refactor
    
    * Fix configuration
    
    * Add more ingress configuration
    
    * Fix AMI creation date
    
    * Refactor
    
    * Add spell check

---
+++ xenon.tf
@@ -0,0 +1,26 @@
+resource "google_compute_instance" "xenon" {
+  name                      = "xenon"
+  machine_type              = "e2-highcpu-4"
+  allow_stopping_for_update = true
+  tags                      = ["http-server"]
+  metadata = {
+    ssh-keys = join(":", [var.ssh_user, var.ssh_public_key])
+  }
+
+  boot_disk {
+    initialize_params {
+      size = 256
+      type = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {}
+  }
+}
+
+output "xenon_ip_address" {
+  value = google_compute_instance.xenon.network_interface.0.access_config.0.nat_ip
+}
