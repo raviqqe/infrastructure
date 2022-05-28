a2bed9e8f3d08f146394a02d76b1967b05a8d87f
Yota Toyama <raviqqe@gmail.com>
2022-05-28 23:04:42 +0000

    Add records

--- dns.tf
+++ dns.tf
@@ -1,3 +1,11 @@
+variable "firebase_records" {
+  default = ["151.101.1.195", "151.101.65.195"]
+}
+
+variable "default_record_ttl" {
+  default = 300
+}
+
 variable "record_ttl" {
   default = "300"
 }
@@ -12,6 +20,30 @@ resource "aws_route53_zone" "code2d_net" {
 
 resource "aws_route53_zone" "code2d_org" {
   name = "code2d.org"
+}
+
+resource "aws_route53_record" "tasks" {
+  zone_id = aws_route53_zone.code2d_org.zone_id
+  name    = "tasks.code2d.org"
+  type    = "A"
+  ttl     = var.default_record_ttl
+  records = [aws_eip.lb.public_ip]
+}
+
+resource "aws_route53_record" "notes" {
+  zone_id = aws_route53_zone.code2d_org.zone_id
+  name    = "notes.code2d.org"
+  type    = "A"
+  ttl     = var.default_record_ttl
+  records = [aws_eip.lb.public_ip]
+}
+
+resource "aws_route53_record" "pomodoro" {
+  zone_id = aws_route53_zone.code2d_org.zone_id
+  name    = "pomodoro.code2d.org"
+  type    = "A"
+  ttl     = var.default_record_ttl
+  records = [aws_eip.lb.public_ip]
 }
 
 resource "aws_route53_zone" "ein_com" {
