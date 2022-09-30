99817777eea0aa21ba1c296432d535fc47504076
Yota Toyama <raviqqe@gmail.com>
2022-09-29 18:59:00 -0700

    Reduce number of cores (#29)
    * Reduce number of cores
    
    * Change public key

--- ssh.tf
+++ ssh.tf
@@ -1,6 +1,6 @@
 locals {
   ssh = {
     user       = "raviqqe"
-    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6+38+Cid9vR4T44jZu5cbY5YatdXS9Sh0vnn7ZDimQ raviqqe@gmail.com"
+    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMfLN0Be60kU1mN+U37QlID82YUiV9jATKxHDME5kX1j raviqqe@MacBook-Air"
   }
 }
