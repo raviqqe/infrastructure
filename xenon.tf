fbb7a8bbe53a55186108aa6e1a136dee3421de58
Yota Toyama <raviqqe@gmail.com>
2022-12-17 10:01:00 -0800

    Fix xenon

--- xenon.tf
+++ xenon.tf
@@ -87,6 +87,6 @@ resource "google_compute_instance" "xenon" {
 }
 
 data "google_compute_image" "ubuntu" {
-  family  = "ubuntu-2204-lts"
   project = "ubuntu-os-cloud"
+  family  = "ubuntu-2204-lts"
 }
