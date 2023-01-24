cafe99e7428b336729ce0d49a5a3e500c52f8715
Yota Toyama <raviqqe@gmail.com>
2023-01-25 00:08:59 +0800

    Refactor

--- github.tf
+++ github.tf
@@ -1,5 +1,6 @@
 module "github_repository" {
-  source       = "./modules/github-repository"
+  source = "./modules/github-repository"
+
   name         = "dictionary"
   description  = "English dictionary with infinite history"
   homepage_url = "https://dictionary.code2d.org/"
