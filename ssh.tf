2f3f698ca58f78e16d6231a5f899a12725a5ecff
Yota Toyama <raviqqe@gmail.com>
2023-01-25 16:49:35 +0800

    Remove source information from SSH public key

--- ssh.tf
+++ ssh.tf
@@ -1,6 +1,6 @@
 locals {
   ssh = {
     user       = "raviqqe"
-    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMfLN0Be60kU1mN+U37QlID82YUiV9jATKxHDME5kX1j raviqqe@MacBook-Air"
+    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMfLN0Be60kU1mN+U37QlID82YUiV9jATKxHDME5kX1j"
   }
 }
