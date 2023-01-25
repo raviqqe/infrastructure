55ab841082ea6fef87034c15f3c99f278acfd41e
Yota Toyama <raviqqe@gmail.com>
2023-01-25 19:16:46 +0800

    Firebase project for InfiniDict (#53)
    Close #47.

---
+++ dictionary.tf
@@ -0,0 +1,26 @@
+module "github_repository" {
+  source = "./modules/github-repository"
+
+  name         = "dictionary"
+  description  = "English dictionary with infinite history"
+  homepage_url = "https://dictionary.code2d.org/"
+  private      = true
+}
+
+resource "random_id" "dictionary_project_id" {
+  byte_length = 5
+  prefix      = "dictionary-"
+}
+
+resource "google_project" "dictionary" {
+  provider = google-beta
+
+  project_id = random_id.dictionary_project_id.id
+  name       = "dictionary"
+}
+
+resource "google_firebase_project" "default" {
+  provider = google-beta
+
+  project = google_project.dictionary.project_id
+}
