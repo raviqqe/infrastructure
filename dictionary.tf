7d178b67968fe3a2a2698a38980bd2b9a3f1c73a
Yota Toyama <raviqqe@gmail.com>
2023-01-29 09:47:45 +0800

    Dictionary infrastructure (#56)
    - Update dictionary
    - Update
    - Fix

--- dictionary.tf
+++ dictionary.tf
@@ -2,7 +2,16 @@ module "github_repository" {
   source = "./modules/github-repository"
 
   name         = "dictionary"
-  description  = "English dictionary with infinite history"
+  description  = "English dictionary for English learners"
   homepage_url = "https://dictionary.code2d.org/"
   private      = true
+}
+
+resource "aws_amplify_app" "dictionary" {
+  name = "dictionary"
+}
+
+resource "aws_amplify_backend_environment" "dictionary" {
+  app_id           = aws_amplify_app.dictionary.id
+  environment_name = "dictionary"
 }
