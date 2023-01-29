5e86f63f21a988582bd9c0a2dddeadad425207d9
Yota Toyama <raviqqe@gmail.com>
2023-01-29 09:36:52 +0800

    Update dictionary

--- dictionary.tf
+++ dictionary.tf
@@ -2,7 +2,19 @@ module "github_repository" {
   source = "./modules/github-repository"
 
   name         = "dictionary"
-  description  = "English dictionary with infinite history"
+  description  = "English dictionary for English learners"
   homepage_url = "https://dictionary.code2d.org/"
   private      = true
+}
+
+resource "aws_amplify_app" "dictionary" {
+  name = "dictionary"
+}
+
+resource "aws_amplify_backend_environment" "dictionary" {
+  app_id           = aws_amplify_app.example.id
+  environment_name = "example"
+
+  deployment_artifacts = "app-example-deployment"
+  stack_name           = "amplify-app-example"
 }
