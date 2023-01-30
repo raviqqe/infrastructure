76771693ab066963258537be9f609b5410e9db02
Yota Toyama <raviqqe@gmail.com>
2023-01-30 08:37:07 +0800

    Revert "Remove app temporarily (#62)" (#63)
    This reverts commit cf864d835e8e07d7d12fa103ab77412fa61a0de6.

--- dictionary.tf
+++ dictionary.tf
@@ -7,12 +7,16 @@ module "github_repository" {
   private      = true
 }
 
-# TODO Revert this back.
-# resource "aws_amplify_app" "dictionary" {
-#   name = "dictionary"
-# }
+resource "aws_amplify_app" "dictionary" {
+  name = "dictionary"
+}
 
-# resource "aws_amplify_backend_environment" "dictionary" {
-#   app_id           = aws_amplify_app.dictionary.id
-#   environment_name = "production"
-# }
+resource "aws_amplify_backend_environment" "dictionary" {
+  app_id           = aws_amplify_app.dictionary.id
+  environment_name = "staging"
+}
+
+resource "aws_amplify_backend_environment" "production" {
+  app_id           = aws_amplify_app.dictionary.id
+  environment_name = "production"
+}
