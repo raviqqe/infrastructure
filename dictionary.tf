d9d65873522425d769f31bdb3cdc86c971d04a9b
Yota Toyama <raviqqe@gmail.com>
2023-01-30 08:35:31 +0800

    Fix

--- dictionary.tf
+++ dictionary.tf
@@ -13,5 +13,10 @@ resource "aws_amplify_app" "dictionary" {
 
 resource "aws_amplify_backend_environment" "dictionary" {
   app_id           = aws_amplify_app.dictionary.id
+  environment_name = "staging"
+}
+
+resource "aws_amplify_backend_environment" "production" {
+  app_id           = aws_amplify_app.dictionary.id
   environment_name = "production"
 }
