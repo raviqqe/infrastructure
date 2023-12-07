59d695d2cda4aa8b5cb483674badfd31868d97f5
Yota Toyama <raviqqe@gmail.com>
2023-12-08 08:56:53 +0900

    Remove modules

--- modules/aws_amplify/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_amplify_app" "main" {
-  name = var.name
-}
-
-resource "aws_amplify_backend_environment" "production" {
-  app_id           = aws_amplify_app.main.id
-  environment_name = "production"
-}
--- modules/github_repository/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "repo_id" {
-  value = github_repository.repository.repo_id
-}
