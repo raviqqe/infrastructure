a45cd2b3b2628c8db676a2f54f0f7453a543db7c
Yota Toyama <raviqqe@gmail.com>
2023-12-18 17:27:35 +0900

    Fix

--- one_rpc.tf
+++ one_rpc.tf
@@ -3,11 +3,6 @@ resource "aws_iam_user" "one_rpc_ci" {
   path = "/system/"
 }
 
-resource "aws_iam_access_key" "one_rpc_ci" {
-  user    = aws_iam_user.one_rpc_ci.name
-  pgp_key = "keybase:some_person_that_exists"
-}
-
 data "aws_iam_policy_document" "one_rpc_ci" {
   statement {
     effect    = "Allow"
@@ -20,6 +15,11 @@ resource "aws_iam_user_policy" "one_rpc_ci" {
   name   = "test"
   user   = aws_iam_user.one_rpc_ci.name
   policy = data.aws_iam_policy_document.one_rpc_ci.json
+}
+
+resource "aws_iam_access_key" "one_rpc_ci" {
+  user    = aws_iam_user.one_rpc_ci.name
+  pgp_key = "keybase:some_person_that_exists"
 }
 
 output "secret" {
