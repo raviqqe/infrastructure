c2aab51a5c8197b14a50bf082c4c20d316f47f16
Yota Toyama <raviqqe@gmail.com>
2023-12-18 17:24:12 +0900

    Set up IAM for CDK

---
+++ cdk_iam.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_access_key" "lb" {
+  user    = aws_iam_user.lb.name
+  pgp_key = "keybase:some_person_that_exists"
+}
+
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+}
+
+data "aws_iam_policy_document" "lb_ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name   = "test"
+  user   = aws_iam_user.lb.name
+  policy = data.aws_iam_policy_document.lb_ro.json
+}
+
+output "secret" {
+  value     = aws_iam_access_key.lb.encrypted_secret
+  sensitive = true
+}
