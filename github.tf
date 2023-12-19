e1b03a5a32f90eb5f0bf804ab7fad8ca470e590e
Yota Toyama <raviqqe@gmail.com>
2023-12-19 23:10:18 +0900

    Set up GitHub OIDC provider on AWS

--- github.tf
+++ github.tf
@@ -2,3 +2,11 @@ resource "github_user_ssh_key" "mac" {
   title = "mac"
   key   = local.ssh.public_key
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [
+    "266362248691-342342xasdasdasda-apps.googleusercontent.com",
+  ]
+  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+}
