201a91efcec0c7cfcf8b12010448ad344eb441c0
Yota Toyama <raviqqe@gmail.com>
2023-12-19 21:22:46 +1100

    Set up IAM for CDK (#80)

---
+++ onerpc.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_user" "onerpc_ci" {
+  name = "onerpc_ci"
+  path = "/system/"
+}
+
+data "aws_iam_policy_document" "onerpc_ci" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "onerpc_ci" {
+  name   = "onerpc_ci"
+  user   = aws_iam_user.onerpc_ci.name
+  policy = data.aws_iam_policy_document.onerpc_ci.json
+}
+
+resource "aws_iam_access_key" "onerpc_ci" {
+  user = aws_iam_user.onerpc_ci.name
+}
+
+output "secret" {
+  value     = aws_iam_access_key.onerpc_ci.encrypted_secret
+  sensitive = true
+}
