57599979e14777614b48e66ec58f49c7d17cfb15
Yota Toyama <raviqqe@gmail.com>
2023-12-19 23:45:42 +0900

    Fix

--- github.tf
+++ github.tf
@@ -6,7 +6,7 @@ resource "github_user_ssh_key" "mac" {
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
   client_id_list = [
-    "266362248691-342342xasdasdasda-apps.googleusercontent.com",
+    "sts.amazonaws.com"
   ]
   thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
 }
