c760aa600a7b16e1f8a0094614ea57cef4667b8b
Yota Toyama <raviqqe@gmail.com>
2023-12-19 23:46:29 +0900

    Fix

--- github.tf
+++ github.tf
@@ -8,5 +8,5 @@ resource "aws_iam_openid_connect_provider" "github" {
   client_id_list = [
     "sts.amazonaws.com"
   ]
-  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
 }
