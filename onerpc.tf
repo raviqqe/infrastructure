5e159e7385b320e257945712782302bd1d1881eb
Yota Toyama <raviqqe@gmail.com>
2023-12-19 21:43:06 +1100

    Use Keybase (#82)
    - Use keybase
    - Fix

--- onerpc.tf
+++ onerpc.tf
@@ -18,10 +18,14 @@ resource "aws_iam_user_policy" "onerpc_ci" {
 }
 
 resource "aws_iam_access_key" "onerpc_ci" {
-  user = aws_iam_user.onerpc_ci.name
+  user    = aws_iam_user.onerpc_ci.name
+  pgp_key = "keybase:raviqqe"
 }
 
-output "secret" {
-  value     = aws_iam_access_key.onerpc_ci.secret
-  sensitive = true
+output "onerpc_ci_access_key_id" {
+  value = aws_iam_access_key.onerpc_ci.id
+}
+
+output "onerpc_ci_secret_access_key" {
+  value = aws_iam_access_key.onerpc_ci.encrypted_secret
 }
