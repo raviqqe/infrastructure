7591a920ebdd25ebaf61dd4c9507b4ec5247cdbd
Yota Toyama <raviqqe@gmail.com>
2023-12-21 21:39:12 +0900

    Fix

--- main.tf
+++ main.tf
@@ -8,8 +8,18 @@ terraform {
   }
 
   required_providers {
+    aws = {
+      source  = "aws"
+      version = "~> 5.0"
+    }
+
     github = {
       source  = "integrations/github"
+      version = "~> 5.0"
+    }
+
+    google = {
+      source  = "google"
       version = "~> 5.0"
     }
   }
