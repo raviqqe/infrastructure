1a272027aafe5a55099744f6c93235e59a98cd72
Yota Toyama <raviqqe@gmail.com>
2024-03-23 16:40:04 +0900

    Fix homepage URL (#131)

--- onerpc.tf
+++ onerpc.tf
@@ -1,8 +1,9 @@
 module "onerpc_repository" {
   source = "git::https://github.com/raviqqe/terraform-modules//github_repository"
 
-  name        = "oneRPC"
-  description = "The router-less serverless RPC framework for TypeScript"
+  name         = "oneRPC"
+  description  = "The router-less serverless RPC framework for TypeScript"
+  homepage_url = "https://raviqqe.github.io/oneRPC"
   topics = [
     "aws-lambda",
     "edge-computing",
