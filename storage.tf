500d242a0c5f30dac329a2b07a3c0ec2b859155d
Yota Toyama <raviqqe@gmail.com>
2025-04-17 18:16:26 -0700

    Fix

--- storage.tf
+++ storage.tf
@@ -3,3 +3,19 @@ resource "google_storage_bucket" "raviqqe" {
   location      = "US"
   storage_class = "COLDLINE"
 }
+
+resource "aws_s3_bucket" "raviqqe" {
+  bucket = "raviqqe"
+}
+
+
+resource "aws_s3_bucket_cors_configuration" "raviqqe" {
+  bucket = aws_s3_bucket.raviqqe.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["https://pen-lang.org", "https://raviqqe.com"]
+    expose_headers  = ["etag"]
+  }
+}
