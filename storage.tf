c90956f461a5a175f29371568f24cc3e726129fd
Yota Toyama <raviqqe@gmail.com>
2025-04-17 18:18:51 -0700

    Fix

--- storage.tf
+++ storage.tf
@@ -19,3 +19,17 @@ resource "aws_s3_bucket_cors_configuration" "raviqqe" {
     expose_headers  = ["etag"]
   }
 }
+
+resource "aws_s3_bucket_intelligent_tiering_configuration" "raviqqe" {
+  bucket = aws_s3_bucket.raviqqe.id
+  name   = "main"
+
+  tiering {
+    access_tier = "DEEP_ARCHIVE_ACCESS"
+    days        = 180
+  }
+  tiering {
+    access_tier = "ARCHIVE_ACCESS"
+    days        = 90
+  }
+}
