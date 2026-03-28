ded739d285a663362d46a26d2b3254fb37b4bab2
Yota Toyama <raviqqe@gmail.com>
2026-03-27 22:28:01 -0700

    Remove ACL block (#209)

--- storage.tf
+++ storage.tf
@@ -42,15 +42,6 @@ resource "aws_s3_bucket_ownership_controls" "raviqqe" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "raviqqe" {
-  bucket = aws_s3_bucket.raviqqe.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket_versioning" "raviqqe" {
   bucket = aws_s3_bucket.raviqqe.id
 
