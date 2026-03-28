ed8df758d4e9005bb42122c0bdd6d0d567f6b9a9
Yota Toyama <raviqqe@gmail.com>
2026-03-27 22:13:36 -0700

    Private bucket (#208)

--- storage.tf
+++ storage.tf
@@ -1,31 +1,22 @@
 resource "google_storage_bucket" "raviqqe" {
-  name          = "raviqqe"
-  location      = "US"
-  storage_class = "COLDLINE"
+  name                        = "raviqqe"
+  location                    = "US"
+  storage_class               = "COLDLINE"
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
 }
 
 resource "aws_s3_bucket" "raviqqe" {
   bucket = "raviqqe"
 }
 
-resource "aws_s3_bucket_acl" "raviqqe" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.raviqqe,
-    aws_s3_bucket_public_access_block.raviqqe,
-  ]
-
-  bucket = aws_s3_bucket.raviqqe.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_cors_configuration" "raviqqe" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "raviqqe" {
   bucket = aws_s3_bucket.raviqqe.id
 
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "HEAD"]
-    allowed_origins = ["https://pen-lang.org", "https://raviqqe.com"]
-    expose_headers  = ["etag"]
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
@@ -47,17 +38,17 @@ resource "aws_s3_bucket_ownership_controls" "raviqqe" {
   bucket = aws_s3_bucket.raviqqe.id
 
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    object_ownership = "BucketOwnerEnforced"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "raviqqe" {
   bucket = aws_s3_bucket.raviqqe.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "raviqqe" {
