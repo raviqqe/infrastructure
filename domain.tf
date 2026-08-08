502d343ded9da6423310e0e4e830e39e4125bbc2
Yota Toyama <raviqqe@gmail.com>
2026-08-08 16:12:38 -0700

    De-register unused domains (#247)

--- domain.tf
+++ domain.tf
@@ -1,7 +1,5 @@
 locals {
-  // cspell: ignore infini infinidict
   domains = [
-    "cloe-lang.com",
     "cloe-lang.org",
     "code2d.net",
     "code2d.org",
@@ -9,9 +7,6 @@ locals {
     "ein-lang.org",
     "flame-lang.com",
     "flame-lang.org",
-    "infini-dict.com",
-    "infinidict.com",
-    "infinity-dictionary.com",
     "pen-lang.com",
     "pen-lang.org",
     "raviqqe.com",
