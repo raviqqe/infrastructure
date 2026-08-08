9e9cf0729d19a8b4345c31ee4bab1a4d2d6795ff
Yota Toyama <raviqqe@gmail.com>
2026-08-08 15:46:17 -0700

    Refactor

--- domain.tf
+++ domain.tf
@@ -1,27 +1,27 @@
 locals {
   // cspell: ignore infini infinidict
-  domain_transfer_locks = {
-    "cloe-lang.com"           = true
-    "cloe-lang.org"           = true
-    "code2d.net"              = true
-    "code2d.org"              = true
-    "ein-lang.com"            = true
-    "ein-lang.org"            = true
-    "flame-lang.com"          = true
-    "flame-lang.org"          = true
-    "infini-dict.com"         = false
-    "infinidict.com"          = false
-    "infinity-dictionary.com" = false
-    "pen-lang.com"            = true
-    "pen-lang.org"            = true
-    "raviqqe.com"             = true
-    "ytoyama.com"             = true
-  }
+  domains = [
+    "cloe-lang.com",
+    "cloe-lang.org",
+    "code2d.net",
+    "code2d.org",
+    "ein-lang.com",
+    "ein-lang.org",
+    "flame-lang.com",
+    "flame-lang.org",
+    "infini-dict.com",
+    "infinidict.com",
+    "infinity-dictionary.com",
+    "pen-lang.com",
+    "pen-lang.org",
+    "raviqqe.com",
+    "ytoyama.com",
+  ]
 }
 
 resource "aws_route53domains_registered_domain" "main" {
-  for_each = local.domain_transfer_locks
+  for_each = toset(local.domains)
 
   domain_name   = each.key
-  transfer_lock = each.value
+  transfer_lock = true
 }
